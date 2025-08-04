1a70b30aa420ae3c986fc4e29571e9bd087e43e4
Dxv-404 <153078528+Dxv-404@users.noreply.github.com>
2025-08-04 12:05:03 +0530

    Removed other key secret files from git

---
+++ frontend/src/firebase_example.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+import { getAuth, RecaptchaVerifier } from "firebase/auth";
+
+export const firebaseConfig = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "your-app.firebaseapp.com",
+  projectId: "your-app",
+  storageBucket: "your-app.appspot.com",
+  messagingSenderId: "YOUR_SENDER_ID",
+  appId: "YOUR_APP_ID",
+};
+
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
